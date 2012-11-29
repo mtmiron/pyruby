fae8d44f115d6756339c2b01fa236807269bd719
mtmiron <mtmiron@mail.rit.edu>
2012-11-29 05:15:02 -0500

    Updating format

--- pyrbmodule.c
+++ pyrbmodule.c
@@ -1,11 +1,11 @@
 /*
  * pyrbmodule.c:
  *
- * 	A Python module that provides native execution of Ruby code
- * 	from directly within the Python interpreter's environment.
+ *  A Python module that provides native execution of Ruby code
+ *  from directly within the Python interpreter's environment.
  *
- * 	Note: return value of pyrb.eval is a Python-ized Ruby object
- * 	(Ruby arrays become Python lists, nil becomes None, etc.)
+ *  Note: return value of pyrb.eval is a Python-ized Ruby object
+ *  (Ruby arrays become Python lists, nil becomes None, etc.)
  *
  *
  * Usage:
@@ -67,32 +67,32 @@ static int rb_needs_init = 1;
 
 // Method definition structure of the module (Python bindings)
 static PyMethodDef pyrb_methods[] = {
-	{ "eval_s", pyrb_eval_s, METH_VARARGS,
-	  "Evaluate a Python string as Ruby code." },
-	{ "eval_f", pyrb_eval_f, METH_VARARGS,
-	  "Evaluate the contents of a file as Ruby code." },
-	{ "eval", pyrb_eval, METH_VARARGS,
-	  "Eval contents of file if filename exists, else eval string as code." },
+    { "eval_s", pyrb_eval_s, METH_VARARGS,
+      "Evaluate a Python string as Ruby code." },
+    { "eval_f", pyrb_eval_f, METH_VARARGS,
+      "Evaluate the contents of a file as Ruby code." },
+    { "eval", pyrb_eval, METH_VARARGS,
+      "Eval contents of file if filename exists, else eval string as code." },
     { "ruby_version", (PyCFunction)pyrb_ruby_version, METH_NOARGS,
       "Return the version of the embedded Ruby interpreter" },
-	{ NULL, NULL, 0, NULL },
+    { NULL, NULL, 0, NULL },
 };
 
 
 // Initialize the embedded Ruby interpreters environment
 static void init_ruby_env(int argc, char **argv)
 {
-	if (!rb_needs_init)
-		return;
+    if (!rb_needs_init)
+        return;
 
-	ruby_init();
-	ruby_init_loadpath();
-	ruby_set_argv(argc, argv);
+    ruby_init();
+    ruby_init_loadpath();
+    ruby_set_argv(argc, argv);
 
-	id_to_s = rb_intern("to_s");
-	id_inspect = rb_intern("inspect");
+    id_to_s = rb_intern("to_s");
+    id_inspect = rb_intern("inspect");
 
-	rb_needs_init = 0;
+    rb_needs_init = 0;
 }
 
 
@@ -124,7 +124,7 @@ static PyObject *rbobject_to_pyobject(VALUE rb_obj)
         return PyString_FromString(RSTRING_PTR(rb_obj));
 
       default:
-		return RBSTR2PYSTR(RB_INSPECT(rb_obj));
+        return RBSTR2PYSTR(RB_INSPECT(rb_obj));
     }
 }
 
@@ -155,15 +155,15 @@ static PyObject *pyrb_ruby_version(PyObject *self)
 // call this func to perform the actual Ruby code evaluation
 static PyObject *pyrb_eval_cstr(PyObject *self, char *arg)
 {
-	const char *rb_code = (const char *)arg;
-	int state = 0;
-	VALUE rb_ret;
+    const char *rb_code = (const char *)arg;
+    int state = 0;
+    VALUE rb_ret;
 
-	rb_ret = rb_eval_string_protect(rb_code, &state);
-	if (state) {
-		PyErr_SetString(pyrb_exc, RSTRING_PTR( RB_GET_ERRINFO() ));
-		return NULL;
-	}
+    rb_ret = rb_eval_string_protect(rb_code, &state);
+    if (state) {
+        PyErr_SetString(pyrb_exc, RSTRING_PTR( RB_GET_ERRINFO() ));
+        return NULL;
+    }
     return rbobject_to_pyobject(rb_ret);
 }
 
@@ -171,32 +171,32 @@ static PyObject *pyrb_eval_cstr(PyObject *self, char *arg)
 // Call the Ruby interpreter to eval/exec a Python string object
 static PyObject *pyrb_eval_s(PyObject *self, PyObject *args)
 {
-	const char *rb_code;
+    const char *rb_code;
 
-	if (! PyArg_ParseTuple(args, "s", &rb_code) )
-		return NULL;
-	else
-		return pyrb_eval_cstr(self, (char *)rb_code);
+    if (! PyArg_ParseTuple(args, "s", &rb_code) )
+        return NULL;
+    else
+        return pyrb_eval_cstr(self, (char *)rb_code);
 }
 
 
 // Call the Ruby interpreter to eval/exec the contents of a file (name passed as Python string)
 static PyObject *pyrb_eval_f(PyObject *self, PyObject *args)
 {
-	const char *fname;
-	VALUE rb_f_contents;
-	
-	if (! PyArg_ParseTuple(args, "s", &fname) )
-		return NULL;
+    const char *fname;
+    VALUE rb_f_contents;
+    
+    if (! PyArg_ParseTuple(args, "s", &fname) )
+        return NULL;
 
-	if (! FILE_EXISTS_P(fname) ) {
-		PyErr_SetString(PyExc_IOError, "file not found");
-		return NULL;
-	}
+    if (! FILE_EXISTS_P(fname) ) {
+        PyErr_SetString(PyExc_IOError, "file not found");
+        return NULL;
+    }
 
-	rb_f_contents = rb_funcall(rb_cFile, rb_intern("read"), 1, rb_str_new2(fname));
-	fname = RSTRING_PTR(rb_funcall(rb_f_contents, id_to_s, 0));
-	return pyrb_eval_cstr(self, (char *)fname);
+    rb_f_contents = rb_funcall(rb_cFile, rb_intern("read"), 1, rb_str_new2(fname));
+    fname = RSTRING_PTR(rb_funcall(rb_f_contents, id_to_s, 0));
+    return pyrb_eval_cstr(self, (char *)fname);
 }
 
 
@@ -206,31 +206,31 @@ static PyObject *pyrb_eval_f(PyObject *self, PyObject *args)
  */
 static PyObject *pyrb_eval(PyObject *self, PyObject *args)
 {
-	char *str;
+    char *str;
 
-	if (! PyArg_ParseTuple(args, "s", &str) )
-		return NULL;
+    if (! PyArg_ParseTuple(args, "s", &str) )
+        return NULL;
 
-	// Check if this is an existing filename & run file contents if so
-	if ( FILE_EXISTS_P(str) )
-		return pyrb_eval_f(self, args);
-	else
-		return pyrb_eval_s(self, args);
+    // Check if this is an existing filename & run file contents if so
+    if ( FILE_EXISTS_P(str) )
+        return pyrb_eval_f(self, args);
+    else
+        return pyrb_eval_s(self, args);
 }
 
 
 // Initialize Python bindings, init the embedded Ruby interpreter, etc.
 PyMODINIT_FUNC initpyrb()
 {
-	PyObject *pyrb_module;
+    PyObject *pyrb_module;
 
-	// Ruby init
-	init_ruby_env(0, NULL);
+    // Ruby init
+    init_ruby_env(0, NULL);
 
-	// Python inits
-	pyrb_module = Py_InitModule("pyrb", pyrb_methods);
+    // Python inits
+    pyrb_module = Py_InitModule("pyrb", pyrb_methods);
 
-	pyrb_exc = PyErr_NewException("pyrb.ruby_error", NULL, NULL);
-	Py_XINCREF(pyrb_exc);
-	PyModule_AddObject(pyrb_module, "exception", pyrb_exc);
+    pyrb_exc = PyErr_NewException("pyrb.ruby_error", NULL, NULL);
+    Py_XINCREF(pyrb_exc);
+    PyModule_AddObject(pyrb_module, "exception", pyrb_exc);
 }
