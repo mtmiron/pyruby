6e3e85aa0602ddbee5d01248ee1d86f097ba4c7f
Murray Miron <mmiron@srllc.org>
2021-03-22 16:26:57 -0400

    Be a little more clear

--- test.py
+++ test.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import pyrb
+import pprint
 
 
-print("This resulted from Python code; assigning Ruby heredoc to a var and eval'ing it...")
-print
+ruby_script_array="""
+    def make_an_array()
+        array = []
+        3.times { |i| array.push(i) }
+        array.push(3.14159265)
+        return array
+    end
 
+    make_an_array()
+"""
 
-ruby_script="""
-puts "This is pure Ruby from within the Python environment."
-puts
-puts "Raising a Ruby exception..."
-raise "Testing_Ruby_Exception_to_Python_Exception"
+ruby_script_exception="""
+    raise "This Exception was raised in Ruby!"
 """
 
 
-pyrb.eval(ruby_script)
+print("\n- Executing this Ruby code:")
+print(ruby_script_array)
+
+list_from_ruby = pyrb.eval(ruby_script_array)
+
+print("\n- The value returned by Ruby to Python is (Ruby returns the value of the last expression by default):")
+print("\n    {}".format(list_from_ruby))
+print("\n\n- Executing this Ruby code:")
+print(ruby_script_exception)
+
+try:
+    pyrb.eval(ruby_script_exception)
+except Exception as exc:
+    print("\n- Caught the exception raised from Ruby in a Python try/catch block!  Re-raising for traceback...\n")
+    raise exc
